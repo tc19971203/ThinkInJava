0b9e200a9800efb7bb91343c6dddeae566855441
tangchen <tc15542726981@163.com>
2021-02-05 16:39:01 +0800

    queue

---
+++ src/holdingObjects/Map/Statistics.java
@@ -0,0 +1,21 @@
+package holdingObjects.Map;
+
+import java.util.HashMap;
+import java.util.Random;
+
+public class Statistics {
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        HashMap<Integer, Integer> m = new HashMap<>();
+
+        for (int i=0; i<10000; i++){
+            int r = rand.nextInt(20);
+
+            Integer freq = m.get(r);
+
+            m.put(r,freq == null ? 1 : freq+1);
+        }
+
+        System.out.println(m);
+    }
+}
---
+++ src/holdingObjects/queue/QueueDemo.java
@@ -0,0 +1,34 @@
+package holdingObjects.queue;
+
+import java.util.*;
+
+public class QueueDemo {
+
+    public static void printQ(Queue queue){
+        while (queue.peek()!=null){
+            System.out.println(queue.remove()+" ");
+        }
+
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        Random rand = new Random(47);
+
+        for (int i = 0 ; i < 10; i++){
+            queue.offer(rand.nextInt(i+10));
+        }
+
+        printQ(queue);
+
+        Queue<Character> qc = new LinkedList<Character>();
+
+        for (char c : "Brotosaurus".toCharArray());
+
+        printQ(qc);
+
+    }
+}
