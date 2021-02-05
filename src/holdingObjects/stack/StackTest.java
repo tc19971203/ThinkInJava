ec9f8551ead1f5ebfa86eb55ef63fa064b22b010
tangchen <tc15542726981@163.com>
2021-02-05 14:27:47 +0800

    栈

---
+++ src/holdingObjects/stack/StackTest.java
@@ -0,0 +1,16 @@
+package holdingObjects.stack;
+
+import java.util.Stack;
+
+public class StackTest {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        for(String s : "My dog has fleas".split(" ")){
+            stack.push(s);
+        }
+
+        while (!stack.empty()){
+            System.out.println(stack.pop());
+        }
+    }
+}
